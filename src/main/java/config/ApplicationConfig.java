4ea61a917497a1e0ae8cfde24bf6a8e6ae78d184
entrenadorcilo <kapkinkirill@gmail.com>
2021-04-02 21:24:21 +0300

    Old version

---
+++ src/main/java/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class ApplicationConfig {
+
+
+}

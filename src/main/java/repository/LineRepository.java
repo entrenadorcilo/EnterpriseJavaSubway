5b5602eecf56c934cdcfab8eac40d2348e6d6c61
entrenadorcilo <kapkinkirill@gmail.com>
2021-04-03 08:05:45 +0300

    Add classes and packages

--- src/main/java/repository/LineRepository.java
+++ src/main/java/repository/LineRepository.java
@@ -1,4 +1,4 @@
-package services;
+package repository;
 
 public interface LineRepository {
 }
--- src/main/java/repository/SubwayRepositoryImpl.java
+++ src/main/java/repository/SubwayRepositoryImpl.java
@@ -1,4 +1,4 @@
-package services;
+package repository;
 
 public class SubwayRepositoryImpl implements SubwayRepository{
 
